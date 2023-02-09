baeb477ac1e20b5fd073ccdb6fe11601c21e137a
euna <christinejung10@daum.net>
2023-02-10 00:21:31 +0900

    feat(chapter03): 챕터 3 코드 구현
    1. StreamBasic : 자바 7 코드와 자바 8 코드 비교
    2. HighCaloriesNames : 중간 연산의 과정 확인

---
+++ src/main/java/chapter04/StreamBasic.java
@@ -0,0 +1,51 @@
+package chapter04;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
+public class StreamBasic {
+
+    public static void main(String[] args) {
+        // 자바 7
+        getLowCaloricDishesNamesInJava7(Dish.menu).forEach(System.out::println);
+
+        System.out.println("---");
+
+        // 자바 8
+        getLowCaloricDishesNamesInJava8(Dish.menu).forEach(System.out::println);
+    }
+
+    public static List<String> getLowCaloricDishesNamesInJava7(List<Dish> dishes) {
+        List<Dish> lowCaloricDishes = new ArrayList<>();
+        for (Dish d : dishes) {
+            if (d.getCalories() < 400) {
+                lowCaloricDishes.add(d);
+            }
+        }
+        List<String> lowCaloricDishesName = new ArrayList<>();
+        Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
+            @Override
+            public int compare(Dish d1, Dish d2) {
+                return Integer.compare(d1.getCalories(), d2.getCalories());
+            }
+        });
+        for (Dish d : lowCaloricDishes) {
+            lowCaloricDishesName.add(d.getName());
+        }
+        return lowCaloricDishesName;
+    }
+
+    public static List<String> getLowCaloricDishesNamesInJava8(List<Dish> dishes) {
+        return dishes.stream()
+                .filter(d -> d.getCalories() < 400) // 400 칼로리 이하의 요리 선택
+                .sorted(comparing(Dish::getCalories)) // 칼로리로 요리 정렬
+                .map(Dish::getName) // 요리명 추출
+                .collect(toList()); // 모든 요리명을 리스트에 저장
+    }
+
+}
